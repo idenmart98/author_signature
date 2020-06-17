d9915cb92bc8cf3731a6639d79faa044316781cb
iden <idenmart07@gmail.com>
2020-06-17 16:57:23 +0600

    fixes

--- script.py
+++ script.py
@@ -4,20 +4,26 @@ import glob
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-listing = os.listdir()
+import click
 
-image_list = []
-font = ImageFont.truetype('fonts/font1.ttf', size=25)
-for filename in glob.glob('source-images/*.jpg'):
-    im = Image.open(filename)
-    draw_text = ImageDraw.Draw(im)
-    name = os.path.basename(filename)
-    index = name.index('.')
-    size = draw_text.textsize(name[:index], font=font)
-    draw_text.text(
-        (im.width-(size[0]+7), im.height-(size[1]+7)),
-        name[:index],
-        font=font,
-        fill=('#808080')
-        ) 
-    im.save('output-images/{}.jpg'.format(name[:index]+'signatured'))
+@click.command()
+@click.option('--way', default='output-images', help='Number of greetings.')
+def signature(way):
+    font = ImageFont.truetype('fonts/font1.ttf', size=25)
+    for filename in glob.glob('source-images/*.jpg'):
+        im = Image.open(filename)
+        draw_text = ImageDraw.Draw(im)
+        name = os.path.basename(filename)
+        index = name.index('.')
+        size = draw_text.textsize(name[:index], font=font)
+        draw_text.text(
+            (im.width-(size[0]+7), im.height-(size[1]+7)),
+            name[:index],
+            font=font,
+            fill=('#808080')
+            )
+        filepath = way+'/'+name 
+        im.save(filepath)
+
+if __name__ == '__main__':
+    signature()
